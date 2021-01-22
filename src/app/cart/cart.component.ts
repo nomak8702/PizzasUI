052242ad0c64026ca443671eed2e1fc32c389df4
Istvan <nomak8702@gmail.com>
2021-01-22 23:31:42 +0200

    cart 5

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {MessengerService} from 'src/app/services/messenger.service';
 import {Product} from 'src/app/models/product';
+import { CartItemComponent } from 'src/app/cart/cart-item/cart-item.component';
 
 @Component({
   selector: 'app-cart',
@@ -16,22 +17,44 @@ cartItems = [
 cartTotal =0;
   constructor(private msg: MessengerService) { }
 
-  ngOnInit(): void {
-
+  ngOnInit() {
 this.msg.getMsg().subscribe((product: Product) =>{
-
-this.cartItems.push({
-  productName: product.name,
-  qty:1,
-  price: product.price
-})
-
-this.cartTotal=0
- this.cartItems.forEach(item =>{
-  this.cartTotal +=(item.qty*item.price)
-
-  })
+this.addProductToCart(product)
 })
   }
+
+ addProductToCart(product: Product){
+
+let productExist = false
+
+  for(let i  in this.cartItems){
+   if(this.cartItems[i].productId === product.id){
+   this.cartItems[i].qty++
+   productExist=true;
+   break;
+
+   }
+   }
+
+ if(!productExist){
+ this.cartItems.push({
+     productId: product.id,
+     productName: product.name,
+     qty:1,
+     price: product.price
+   })
+ }
+
+
+
+
+ this.cartTotal=0
+  this.cartItems.forEach(item =>{
+   this.cartTotal +=(item.qty*item.price)
+
+   })
+
+
+ }
 
 }
